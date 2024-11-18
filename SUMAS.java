09f11382732c82aee889d3b8f76fbb42b9b920bc
derekperez27 <derekpp2016@gmail.com>
2024-11-18 17:07:05 +0100

    Add files via upload

---
+++ SUMAS.java
@@ -0,0 +1,28 @@
+package operaciones;
+
+public class SUMAS {
+	
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			int[] sumArray = {4,36,4,23,867};
+			int[] subArray = {785675, 56, 5675, 56544, 234};
+			int[] prodArray = {6, 8, 20, 23, 12};
+			
+			// User 1 fer la suma dels elements de sumArray
+			
+			
+		int suma = 0;
+		
+		for( int i = 0; i < sumArray.length; i++) {
+			
+			suma += sumArray[i];
+		}
+		System.out.println("la suma da:" + suma);
+		
+
+		
+
+	}
+}
+
